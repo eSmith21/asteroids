dd772b2f838d13315a40023f2243134b007c8657
Edward Smith <ed.smith21@icloud.com>
2024-10-24 10:49:24 -0400

    Created Player, inherited parent class, instantiate Player object; center screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
-	
+from player import Player
+
+
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
@@ -10,13 +12,21 @@ def main():
 	print("Screen width: 1280")
 	print("Screen height: 720")
 
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+
+		player.draw(screen)
+
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
+
+
+
 
 
 if __name__ == "__main__":
