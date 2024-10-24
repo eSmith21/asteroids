64291ae83fec1a3cc5cebe5e790a006d373ea1b3
Edward Smith <ed.smith21@icloud.com>
2024-10-23 20:27:03 -0400

    Initial commit: Set up game window with basic game loop

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+	
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print("Starting asteroids!")
+	print("Screen width: 1280")
+	print("Screen height: 720")
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		screen.fill((0,0,0))
+		pygame.display.flip()
+
+if __name__ == "__main__":
+	main()
