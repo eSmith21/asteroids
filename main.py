1f7f2b6b442f41f4f0dc94bfe37f88204411b7c9
Edward Smith <ed.smith21@icloud.com>
2024-10-24 09:48:52 -0400

    Added time.clock()/delta time to use less CPU resources

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 	
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	print("Starting asteroids!")
 	print("Screen width: 1280")
@@ -14,6 +16,8 @@ def main():
 				return
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
 	main()
